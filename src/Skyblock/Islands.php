9bc048d0370dc630676d10ebc259288e48878378
PaulchenHD <paulchen_hd@web.de>
2017-09-30 22:23:29 +0200

    Update Islands.php

--- src/Skyblock/Islands.php
+++ src/Skyblock/Islands.php
@@ -2,22 +2,31 @@
 
 namespace Skyblock;
 
+use pocketmine\level\Position;
+use pocketmine\level\Level;
+use pocketmine\Player;
 use Skyblock\Main;
 
-class Islands {
-    function setWorld($src,$dst) {
+class Islands
+{
+    public function setWorld($src, $dst)
+    {
         $dir = opendir($src);
         @mkdir($dst);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' )) {
-                if ( is_dir($src . '/' . $file) ) {
-                    $this->setWorld($src . '/' . $file,$dst . '/' . $file);
-                }
-                else {
-                    copy($src . '/' . $file,$dst . '/' . $file);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    $this->setWorld($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
                 }
             }
         }
         closedir($dir);
+    }
+    public function tpPlayerToHome(Player $player){
+        $world = $this->getServer()->getLevelByName("sb[". $player->getName(). "]");
+        $loc = $world->getSpawnLocation();
+        $player->teleport(new Position($loc->x, $loc->y, $loc->z, $world));
     }
 }
