6d8b4bde276edaff3dc1be13bb45aa3ecff61c4a
DecodedGaming <BouncyJeffer@gmail.com>
2017-09-30 13:28:59 -0700

    Forgot something, smh

--- src/Skyblock/EventListener.php
+++ src/Skyblock/EventListener.php
@@ -6,7 +6,7 @@ use pocketmine\event\player\PlayerChatEvent;
 class EventListener extends Listener {
     private $main;
     public function __construct(Main $main){
-        $this->main->getPluginManager()->registerEvents($this, $this);
+        $this->main->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     public function onChat(PlayerChatEvent $event){
         // nothing yet   
