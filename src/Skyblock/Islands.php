85683b4dbd7dc223b480e5f73a49468c153f0f04
PaulchenHD <paulchen_hd@web.de>
2017-09-30 20:56:15 +0200

    Add files via upload

---
+++ src/Skyblock/Islands.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Skyblock;
+
+use Skyblock\Main;
+
+class Islands {
+    function setWorld($src,$dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    $this->setWorld($src . '/' . $file,$dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+}
