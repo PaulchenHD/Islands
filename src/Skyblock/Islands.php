a7b0b982b3b70733e5a59eaa1a658808a405196c
DecodedGaming <BouncyJeffer@gmail.com>
2017-09-30 14:51:59 -0700

    Got rid of spacing..

--- src/Skyblock/Islands.php
+++ src/Skyblock/Islands.php
@@ -3,80 +3,47 @@
 namespace Skyblock;
 
 use pocketmine\level\Position;
-
 use pocketmine\level\Level;
-
 use pocketmine\Player;
-
 use Skyblock\Main;
-
-class Islands
-
-{
+class Islands {
 
     public function getMain(){
-
         return new Main();
-
     }
 
     public function deleteIsland(Player $player){
-
         if($this->isIsland($player)) {
-
-            rmdir($this->getMain()->getServer()->getDataPath(). "worlds/sb[". $player->getName()."]");strtolower("A'";
+            rmdir($this->getMain()->getServer()->getDataPath(). "worlds/sb[". $player->getName()."]");
         }
-
     }
 
     public function isIsland(Player $player){
-
         $potentialIsland = $this->getMain()->getServer()->getDataPath(). "worlds/sb[". $player->getName()."]";
-
         if(is_dir($potentialIsland)) {
             return true;
         } else {
             return false;
         }
-
     }
 
-    public function setWorld($src, $dst)
-
-    {
-
+    public function setWorld($src, $dst){
         $dir = opendir($src);
-
         @mkdir($dst);
-
         while (false !== ($file = readdir($dir))) {
-
             if (($file != '.') && ($file != '..')) {
-
                 if (is_dir($src . '/' . $file)) {
-
                     $this->setWorld($src . '/' . $file, $dst . '/' . $file);
-
                 } else {
-
                     copy($src . '/' . $file, $dst . '/' . $file);
-
                 }
-
             }
-
         }
-
         closedir($dir);
-
     }
-
+    
     public function tpPlayerToHome(Player $player, Level $world){
-
         $loc = $world->getSpawnLocation();
-
         $player->teleport(new Position($loc->x, $loc->y, $loc->z, $world));
-
     }
-
 }
